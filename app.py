b896836f38eff790cefb0dc73888a676f825e6d1
stargym0205-8500 <stargym0205@gmail.com>
2026-06-11 12:22:28 +0530

    Re-commit form enhancements after reset

--- app.py
+++ app.py
@@ -15,5 +15,14 @@ def api():
 def todo():
     return render_template('todo.html')
 
+@app.route('/submittodoitem', methods=['POST'])
+def submit_todo_item():
+    item = {
+        "itemName": request.form.get("itemName"),
+        "itemDescription": request.form.get("itemDescription")
+    }
+    collection.insert_one(item)
+    return jsonify({"message": "Item submitted successfully!", "status": "success"})
+
 if __name__ == '__main__':
     app.run(debug=True)
